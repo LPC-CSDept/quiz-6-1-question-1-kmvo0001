9dc7fea1c13bfd3042bb92f47763ed52b4b390be
Kenton Vo <kmvo0001@zonemail.clpccd.edu>
2023-04-10 20:53:02 -0700

    1

--- q1.hpp
+++ q1.hpp
@@ -9,3 +9,23 @@ using namespace std;
 // ******************************
 // Implement all your functions here
 // ******************************
+
+void getinput(int &a,int &b){
+    cin>>a>>b;
+}
+
+void swapTwoValues(int &a, int &b){
+    int temp=a;
+    a=b;
+    b=a;
+}
+
+void getinput(float &a,float &b){
+    cin>>a>>b;
+}
+
+void swapTwoValues(float &a, float&b){
+    float temp=a;
+    a=b;
+    b=temp;
+}
